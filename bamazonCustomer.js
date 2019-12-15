043b3f9fd043999f63cd0b76b36f02a6831e53f7
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-12-14 21:19:11 -0500

    added cli-table

---
+++ bamazonCustomer.js
@@ -0,0 +1,35 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const Table = require("cli-table")
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "V4mp1r3z",
+    database: "bamazon"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    startUp();
+});
+
+function startUp() {
+    let table = new Table ({
+        head: ["ID #", "Product Name", "Department", "Price", "Stock"],
+        colWidths: [7, 50, 25, 10, 7]
+    });
+    con.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+    for (let i = 0; i < result.length; i ++) {
+    table.push([
+        result[i].item_id,
+        result[i].product_name,
+        result[i].department_name,
+        result[i].price,
+        result[i].stock_quantity
+    ])
+    };
+    console.log(table.toString());
+    }
+    )}
